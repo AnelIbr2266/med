e000479bbeaa7419980920191aec944b092367e4
Anel <anelibraeva455@gmail.com>
2025-06-01 22:27:19 +0300

    Папка assets

---
+++ assets/images/index.ts
@@ -0,0 +1,5 @@
+export const MARKER_GREEN = require('./map/marker-green.png');
+export const MARKER_GREEN_DOT = require('./map/marker-green-dot.png');
+export const MARKER_BLUE = require('./map/marker-blue.png');
+export const MARKER_BLUE_DOT = require('./map/marker-blue-dot.png');
+export const MARKER_VIOLET_DOT = require('./map/marker-violet-dot.png');
